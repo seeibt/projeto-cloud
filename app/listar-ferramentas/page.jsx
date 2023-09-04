81c282b6792e7e58296319c4c9620ce3340649c2
Eduardo Seibt <seibteduardo@gmail.com>
2023-09-04 14:33:48 -0300

    changes

--- app/listar-ferramentas/page.jsx
+++ app/listar-ferramentas/page.jsx
@@ -28,7 +28,33 @@ export default async function ListTools(){
 
     return (
         <>
-          
+          {tools.length === 0 ? (
+            <div className="text-red-500 font-semibold">Nenhuma ferramenta cadastrada.</div>
+          ) : (
+            tools.map((tool) => (
+              <div key={tool._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5">
+                <div>
+                  <h1 className="font-bold text-2xl">{tool.nome}</h1>
+                  <div className="">
+                    {tool.posicao} Data: {tool.dataOperacao}
+                  </div>
+                </div>
+      
+                <div className="flex gap-2 items-center">
+                  <RemoveBtn id={tool._id} />
+                    <Link href={`/editar-ferramenta/${tool._id}`}>
+                        <HiPencilAlt size={24} />
+                    </Link>
+                    <Link alt="Retirar Ferramenta" href={`/retirar-ferramenta/${tool._id}`}>
+                        <HiArrowNarrowUp size={24} />
+                    </Link>
+                    <Link alt="Devolver Ferramenta" href={`/devolver-ferramenta/${tool._id}`}>
+                        <HiArrowNarrowDown size={24} />
+                    </Link>
+                </div>
+              </div>
+            ))
+          )}
         </>
       );
 }
