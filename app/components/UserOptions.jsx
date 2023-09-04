399334c402e344fe36c0598f26b26a9c5b8a3615
Eduardo Seibt <seibteduardo@gmail.com>
2023-09-04 16:08:36 -0300

    fix: home page

--- app/components/UserOptions.jsx
+++ app/components/UserOptions.jsx
@@ -3,6 +3,26 @@ import Link from "next/link";
 export default function UserOptions() {
     return(
         <>
+            <div className="text-center">
+                <h1 className="text-4xl font-semibold mb-8">Tela Principal</h1>
+                <div className="space-y-4">
+                    <div className="bg-gray-500 hover:bg-gray-600 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
+                        <Link href="/listar-ferramentas">
+                            Listar Ferramentas
+                        </Link>
+                    </div>
+                    <div className="bg-black hover:bg-gray-900 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
+                        <Link href="/listar-usuarios">
+                            Listar Usuários
+                        </Link>
+                    </div>
+                    <div className="bg-gray-700 hover:bg-gray-800 text-white font-bold py-2 px-4 rounded-full transition duration-300 ease-in-out">
+                        <Link href="/listar-logs">
+                            Listar Logs
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
