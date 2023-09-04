e595b22d57da4a0794a2e87f9be1c6ff07356724
Eduardo Seibt <seibteduardo@gmail.com>
2023-09-04 14:36:31 -0300

    logs page

--- app/listar-logs/page.jsx
+++ app/listar-logs/page.jsx
@@ -24,7 +24,22 @@ export default async function ListLogs(){
 
     console.log(logs)
 
-    const logComponent = <></>
+    const logComponent = logs.map((log) => {
+        const ferramenta = JSON.parse(log.ferramenta)
+        const usuario = JSON.parse(log.usuario)
+
+        console.log(log.usuario)
+
+        return(
+            <div key={log._id} className="p-4 border border-slate-300 my-3 flex justify-between gap-5">
+                <div>
+                    <div className="">
+                        <b>{ferramenta.nome}</b> {log.tipoOperacao} em: <span>{log.dataOperacao} da posição {ferramenta.posicao}</span> por <b> {usuario.nome} </b>
+                    </div>
+                </div>
+            </div>
+        )
+    })
 
     return(
         <div>
