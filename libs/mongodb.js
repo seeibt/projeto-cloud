06f3a0f5201f0fc26f9c50df5ba8c54912b0ad05
Eduardo Seibt <seibteduardo@gmail.com>
2023-10-30 09:07:51 -0300

    fix

--- libs/mongodb.js
+++ libs/mongodb.js
@@ -1,43 +1,13 @@
-import express from 'express';
-import next from 'next';
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
-const dev = process.env.NODE_ENV !== 'production';
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const connectDB = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URL);
+        console.log("MongoDB conectado com sucesso!");
+    } catch(error) {    
+        console.log(error);
+        process.exit(1);
+    }
+}
 
-const port = process.env.PORT || 3000;
-
-app.prepare().then(() => {
-  const server = express();
-
-  // Configuração do CORS para permitir solicitações de uma origem específica
-  server.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000'); // Substitua pela origem permitida
-    res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
-
-  // Configuração da conexão com o MongoDB
-  mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-    .then(() => {
-      console.log('MongoDB conectado com sucesso!');
-    })
-    .catch((error) => {
-      console.error('Erro na conexão com o MongoDB:', error);
-      process.exit(1);
-    });
-
-  server.all('*', (req, res) => {
-    return handle(req, res);
-  });
-
-  server.listen(port, (err) => {
-    if (err) throw err;
-    console.log(`Servidor Next.js rodando na porta ${port}`);
-  });
-});
+export default connectDB;
