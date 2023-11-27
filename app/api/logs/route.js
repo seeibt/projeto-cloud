2c6cd6fd6105a2ed3ea7e013b5c8d50706b14d0a
Eduardo Seibt <seibteduardo@gmail.com>
2023-11-27 16:23:00 -0300

    some changes

--- app/api/logs/route.js
+++ app/api/logs/route.js
@@ -2,11 +2,11 @@ import { NextResponse } from "next/server";
 import connectDB from "../../../libs/mongodb";
 import Log from "@/models/log";
 
-export async function GET(){
-   
+export async function GET() {
     await connectDB();
 
-    const logs = await Log.find({});
+    // Obtém os logs ordenados por dataOperacao em ordem decrescente diretamente do banco de dados
+    const logs = await Log.find({}).sort({ dataOperacao: -1 });
 
-    return NextResponse.json(logs, {status: 200});
+    return NextResponse.json(logs, { status: 200 });
 }
