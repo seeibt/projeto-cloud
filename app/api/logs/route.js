5e23a58f49c478a9b41bda68a5fe95695c3bb135
Eduardo Seibt <seibteduardo@gmail.com>
2023-11-27 16:29:38 -0300

    fix

--- app/api/logs/route.js
+++ app/api/logs/route.js
@@ -5,7 +5,6 @@ import Log from "@/models/log";
 export async function GET() {
     await connectDB();
 
-    // Obtém os logs ordenados por dataOperacao em ordem decrescente diretamente do banco de dados
     const logs = await Log.find({}).sort({ dataOperacao: -1 });
 
     return NextResponse.json(logs, { status: 200 });
