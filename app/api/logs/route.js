4e2a9d550606496c44eb068f157f822753bd3fb4
Eduardo Seibt <seibteduardo@gmail.com>
2023-11-27 16:46:39 -0300

    slow build

--- app/api/logs/route.js
+++ app/api/logs/route.js
@@ -5,6 +5,7 @@ import Log from "@/models/log";
 export async function GET() {
     await connectDB();
 
+    // sei la rs
     const logs = await Log.find({}).sort({ dataOperacao: -1 });
 
     return NextResponse.json(logs, { status: 200 });
